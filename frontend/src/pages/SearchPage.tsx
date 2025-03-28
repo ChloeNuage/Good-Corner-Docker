d979d245dd75e1e819887e6617ecb3821bf8a22e
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2025-03-28 10:58:20 +0100

    Search page

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,8 +1,29 @@
-import { useParams } from "react-router";
+import axios from "axios";
+import { Link, useParams } from "react-router";
+import { useState, useEffect } from "react";
+import AdCard, { AdCardProps } from "../components/AdCard";
 
 const SearchPage = () => {
   const { keyword } = useParams();
-  console.log("keyword", keyword);
-  return <p></p>;
+  const [ads, setAds] = useState<AdCardProps[]>([]);
+  useEffect(() => {
+    const fetchAds = async () => {
+      const result = await axios.get(
+        `http://localhost:3000/ads?search=${keyword}`
+      );
+      console.log("result", result);
+      setAds(result.data);
+    };
+    fetchAds();
+  }, [keyword]);
+  return (
+    <section className="recent-ads">
+      {ads.map((ad) => (
+        <Link key={ad.id} to={`/ads/${ad.id}`}>
+          <AdCard picture={ad.picture} price={ad.price} title={ad.title} />
+        </Link>
+      ))}
+    </section>
+  );
 };
 export default SearchPage;
