560c9c0f1aa563fb1ff680b8211184c515bc5aa1
LoicBrassart <loic.brassart@gmail.com>
2025-04-24 10:09:50 +0200

    Add decorators to one of my entities

--- backend/src/entities/Ad.ts
+++ backend/src/entities/Ad.ts
@@ -10,31 +10,41 @@ import {
 } from "typeorm";
 import Category from "./Category";
 import Tag from "./Tag";
+import { Field, ObjectType } from "type-graphql";
 
 @Entity()
+@ObjectType()
 class Ad extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field()
   id: number;
 
   @Column()
+  @Field()
   title: string;
 
   @Column()
+  @Field()
   description: string;
 
   @Column()
+  @Field()
   owner: string;
 
   @Column()
+  @Field()
   price: number;
 
   @CreateDateColumn()
+  @Field()
   createdAt: Date;
 
   @Column()
+  @Field()
   picture: string;
 
   @Column()
+  @Field()
   location: string;
 
   @ManyToOne(() => Category, (category) => category.ads, { eager: true })
